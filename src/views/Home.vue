d56d00ce16a47a00e866eab54e1ab2e4d086b80d
Wahyu Budi Saputra <wahyubucil@gmail.com>
2019-02-18 17:47:46 +0800

    Init app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+  },
+  data () {
+    return {
+      mahasiswa: [
+        { tinggiBadan: 163, beratBadan: 59, lingkarLenganBawah: 14 },
+        { tinggiBadan: 170, beratBadan: 125, lingkarLenganBawah: 19 },
+        { tinggiBadan: 164, beratBadan: 53, lingkarLenganBawah: 15 },
+        { tinggiBadan: 166, beratBadan: 58, lingkarLenganBawah: 16 },
+        { tinggiBadan: 167, beratBadan: 50, lingkarLenganBawah: 13 },
+        { tinggiBadan: 168, beratBadan: 50, lingkarLenganBawah: 14 },
+        { tinggiBadan: 173, beratBadan: 56, lingkarLenganBawah: 15 },
+        { tinggiBadan: 168, beratBadan: 73, lingkarLenganBawah: 18 },
+        { tinggiBadan: 177, beratBadan: 60, lingkarLenganBawah: 15 },
+        { tinggiBadan: 168, beratBadan: 52, lingkarLenganBawah: 15 },
+        { tinggiBadan: 159, beratBadan: 58, lingkarLenganBawah: 15 },
+        { tinggiBadan: 167, beratBadan: 75, lingkarLenganBawah: 16 },
+        { tinggiBadan: 170, beratBadan: 72, lingkarLenganBawah: 16 },
+        { tinggiBadan: 172, beratBadan: 68, lingkarLenganBawah: 15 },
+        { tinggiBadan: 165, beratBadan: 73, lingkarLenganBawah: 18 },
+        { tinggiBadan: 169.5, beratBadan: 55, lingkarLenganBawah: 14 },
+        { tinggiBadan: 160, beratBadan: 54, lingkarLenganBawah: 15 },
+        { tinggiBadan: 173, beratBadan: 56, lingkarLenganBawah: 14 },
+        { tinggiBadan: 162, beratBadan: 54, lingkarLenganBawah: 15 },
+        { tinggiBadan: 169, beratBadan: 79, lingkarLenganBawah: 17 },
+      ]
+    }
   }
 }
 </script>
